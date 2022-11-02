aee255173894d0c35791b412027913b65d04e296
Prabakaran <99709233+saiMANOPRABA@users.noreply.github.com>
2022-11-02 15:35:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+    setTimeout(function () {
+    var a = document.createElement("div");
+    a.setAttribute("class","a")
+    a.innerText = "10"
+    document.body.appendChild(a);
+
+        setTimeout(function () {
+        var a = document.createElement("div");
+        a.setAttribute("class","a")
+        a.innerText = "9"
+        document.body.appendChild(a);
+
+            setTimeout(function () {
+            var a = document.createElement("div");
+            a.setAttribute("class","a")
+            a.innerText = "8"
+            document.body.appendChild(a);
+
+                 setTimeout(function () {
+                var a = document.createElement("div");
+                a.setAttribute("class","a")
+                a.innerText = "7"
+                document.body.appendChild(a);
+
+                    setTimeout(function () {
+                    var b = document.createElement("div");
+                    b.setAttribute("class","b")
+                    b.innerText = "6"
+                    document.body.appendChild(b);
+
+                    setTimeout(function () {
+                        var b = document.createElement("div");
+                        b.setAttribute("class","b")
+                        b.innerText = "5"
+                        document.body.appendChild(b);
+
+                        setTimeout(function () {
+                            var c = document.createElement("div");
+                            c.setAttribute("class","c")
+                            c.innerText = "4"
+                            document.body.appendChild(c);
+
+                            setTimeout(function () {
+                                var c = document.createElement("div");
+                                c.setAttribute("class","c")
+                                c.innerText = "3"
+                                document.body.appendChild(c);
+
+                                setTimeout(function () {
+                                    var c = document.createElement("div");
+                                    c.setAttribute("class","c")
+                                    c.innerText = "2"
+                                    document.body.appendChild(c);
+
+                                    setTimeout(function () {
+                                        var c = document.createElement("div");
+                                        c.setAttribute("class","c")
+                                        c.innerText = "1"
+                                        document.body.appendChild(c);
+
+
+                                        setTimeout(function () {
+                                            var d = document.createElement("div");
+                                            d.setAttribute("class","d")
+                                            d.innerHTML = "Happy Independence Day"
+                                            document.body.appendChild(d);
+    
+    
+                                        }, 1000)
+
+
+                                    }, 1000)
+
+                                }, 1000)
+
+                            }, 1000)
+
+                        }, 1000)
+
+                    }, 1000)
+
+                }, 1000)
+                
+            }, 1000)
+            
+        }, 1000)
+        
+    }, 1000)
+}, 1000)
